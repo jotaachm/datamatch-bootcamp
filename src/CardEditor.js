0a5f9b062119184fb512353a5592f599f0d0b714
Jota Chamorro <jotachmrro@gmail.com>
2021-11-06 12:34:42 -0400

    Empty flashcards bug fixed

--- src/CardEditor.js
+++ src/CardEditor.js
@@ -9,8 +9,10 @@ class CardEditor extends React.Component {
     }
 
     addCard = () => {
-        this.props.addCard(this.state);
-        this.setState({ front: '', back: '' });
+        if (this.state.front.trim() !== '' && this.state.back.trim() !== '') {
+            this.props.addCard(this.state);
+            this.setState({ front: '', back: '' });
+        }
     }
 
     deleteCard = index => this.props.deleteCard(index);
